4701570fb7ccced912775ca8f979ee7c2118d14d
Ming Liang <khongmingliang@icloud.com>
2021-06-28 13:15:19 +0800

    updated list and details page

--- lib/screens/detailsScreen.dart
+++ lib/screens/detailsScreen.dart
@@ -13,13 +13,41 @@ class DetailsScreen extends StatelessWidget {
         title: Text('Details Screen'),
       ),
       body: Center(
-        child: Column(
-          children: [
-            myImage,
-            Text('Quick Math: You have chosen ${index.toString()}. '
-                '2 + ${index.toString()} is ${(index + 2).toString()}'
-                ' - 1 that\'s ${(index + 2 - 1).toString()}'),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Card(
+                child: myImage,
+                margin: EdgeInsets.all(5.0),
+                elevation: 3,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                child: Table(
+                  columnWidths: {
+                    0: FractionColumnWidth(.2),
+                    1: FractionColumnWidth(.3),
+                  },
+                  children: [
+                    TableRow(children: [
+                      Text('Name'),
+                      Text('Product $index'),
+                    ]),
+                    TableRow(children: [
+                      Text('Discount'),
+                      Text('5%'),
+                    ]),
+                    TableRow(children: [
+                      Text('Validity'),
+                      Text('6 Sept 2021'),
+                    ])
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/screens/listView.dart
+++ lib/screens/listView.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:native_perf/screens/detailsScreen.dart';
 
 class MyListView extends StatelessWidget {
-  final items = List<String>.generate(10000, (i) => "Item $i");
+  final items = List<String>.generate(10000, (i) => "Deal $i");
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +22,8 @@ class MyListView extends StatelessWidget {
             ),
             key: Key('${items[index]}'),
             title: Text('${items[index]}'),
+            leading: Icon(Icons.restaurant),
+            subtitle: Text('Available at location'),
           );
         },
       ),
