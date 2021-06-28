b3a90b86537c0daf2768dd96b20e7a23259af414
Achref Marzouki <amarzouki@au.palo-it.com>
2021-06-28 17:26:54 +1000

    update the detail view

--- lib/screens/detailsScreen.dart
+++ lib/screens/detailsScreen.dart
@@ -3,6 +3,7 @@ import 'package:transparent_image/transparent_image.dart';
 
 class DetailsScreen extends StatelessWidget {
   DetailsScreen({required this.index});
+
   final int index;
 
   @override
@@ -11,48 +12,50 @@ class DetailsScreen extends StatelessWidget {
       placeholder: kTransparentImage,
       image: 'https://picsum.photos/200?image=$index',
     );
+    final titleTextStyle = TextStyle(fontSize: 20, fontWeight: FontWeight.bold);
+    final contentTextStyle = TextStyle(fontSize: 20);
     return Scaffold(
       appBar: AppBar(
         title: Text('Details Screen'),
       ),
-      body: Center(
+      body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(10.0),
+          padding: const EdgeInsets.all(30),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Card(
-                child: SizedBox(
-                  height: 200,
-                  width: 200,
-                  child: myImage,
-                ),
-                margin: EdgeInsets.all(5.0),
-                elevation: 3,
-              ),
               Container(
-                margin: EdgeInsets.symmetric(vertical: 20),
-                child: Table(
-                  columnWidths: {
-                    0: FractionColumnWidth(.2),
-                    1: FractionColumnWidth(.3),
-                  },
-                  children: [
-                    TableRow(children: [
-                      Text('Name'),
-                      Text('Product $index'),
-                    ]),
-                    TableRow(children: [
-                      Text('Discount'),
-                      Text('5%'),
-                    ]),
-                    TableRow(children: [
-                      Text('Validity'),
-                      Text('6 Sept 2021'),
-                    ])
-                  ],
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade100,
+                  borderRadius: BorderRadius.all(Radius.circular(5)),
+                  boxShadow: [BoxShadow(blurRadius: 10)],
+                  image: DecorationImage(
+                    image: myImage.image,
+                    fit: BoxFit.fitWidth,
+                  ),
                 ),
-              )
+              ),
+              SizedBox(height: 16),
+              Table(
+                columnWidths: {
+                  0: FractionColumnWidth(.4),
+                  1: FractionColumnWidth(.6),
+                },
+                children: [
+                  TableRow(children: [
+                    Text('Name', style: titleTextStyle,),
+                    Text('Product $index', style: contentTextStyle,),
+                  ]),
+                  TableRow(children: [
+                    Text('Discount', style: titleTextStyle,),
+                    Text('5%', style: contentTextStyle,),
+                  ]),
+                  TableRow(children: [
+                    Text('Validity', style: titleTextStyle,),
+                    Text('6 Sept 2021', style: contentTextStyle,),
+                  ])
+                ],
+              ),
             ],
           ),
         ),
